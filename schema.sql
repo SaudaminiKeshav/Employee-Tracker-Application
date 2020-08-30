57224a3d93fa09ce67a9fa6c54952722692b8174
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-08-29 19:31:18 -0700

    Added database tables and database connection code

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS company_DB;
+CREATE DATABASE company_DB;
+USE company_DB;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL(15,2) NOT NULL,
+  department_id INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+); 
